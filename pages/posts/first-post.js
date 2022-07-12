e8ceac09473617c189b4caf697598141181423e5
Avi Patel <avi.patel0025@gmail.com>
2022-07-11 23:36:13 -0400

    setup layout styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,23 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
+
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => {
+            console.log(
+              `script loaded correctly, window.FB has been populated`
+            );
+          }}
+        />
       </Head>
       <h1>First Post</h1>
       <h2>
@@ -13,6 +25,6 @@ export default function FirstPost() {
           <a>Back to Home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
